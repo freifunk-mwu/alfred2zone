2a6071db7d2292af7e3d47c3f382ed45cf8aed4e
kokel <tobias@hachmer.de>
2015-03-24 07:14:05 +0100

    update.sh: use correct python script

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export LC_ALL=en_US.UTF-8
 cd /home/admin/clones/alfred2zone/
-/home/admin/bin/alfred-json -s /var/run/alfred-wi.sock -z -r 158 | /usr/bin/python3 alfred2zone-wi.py wi > /var/lib/bind/ffwi/nodes.ffwi.org.master.db
-/home/admin/bin/alfred-json -s /var/run/alfred-mz.sock -z -r 158 | /usr/bin/python3 alfred2zone-mz.py mz > /var/lib/bind/ffmz/nodes.ffmz.org.master.db
+/home/admin/bin/alfred-json -s /var/run/alfred-wi.sock -z -r 158 | /usr/bin/python3 alfred2zone.py wi > /var/lib/bind/ffwi/nodes.ffwi.org.master.db
+/home/admin/bin/alfred-json -s /var/run/alfred-mz.sock -z -r 158 | /usr/bin/python3 alfred2zone.py mz > /var/lib/bind/ffmz/nodes.ffmz.org.master.db
 
 sudo /usr/sbin/rndc reload
